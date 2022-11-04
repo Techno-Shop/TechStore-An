35e681b5a44d0f32a0c078a7b326386ebcd7c3da
aymensouissi <aymensouissi@gmail.com>
2022-11-04 22:16:12 +0100

    carrosel footer navbar

--- TechStore-An/src/app/app-routing.module.ts
+++ TechStore-An/src/app/app-routing.module.ts
@@ -5,9 +5,12 @@ import { LoginComponent } from './auth/login/login.component';
 import { AddComponent } from './admin/add/add.component';
 import { DashboardComponent } from './admin/dashboard/dashboard.component';
 import { ProductsComponent } from './admin/products/products.component';
-import { SidebarComponent } from './admin/sidebar/sidebar.component';
+// import { SidebarComponent } from './admin/sidebar/sidebar.component';
 import { UsersComponent } from './admin/users/users.component';
+import { MainHomeComponent } from './home/main-home/main-home.component';
 const routes: Routes = [
+  {path: "", component: MainHomeComponent},
+
   {path:"register", component: RegisterComponent},
   {path:"login", component: LoginComponent},
   {
--- TechStore-An/src/app/app.module.ts
+++ TechStore-An/src/app/app.module.ts
@@ -1,4 +1,6 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -7,18 +9,16 @@ import { RegisterComponent } from './auth/register/register.component';
 import { LoginComponent } from './auth/login/login.component';
 import { ParamsroutingService } from './services/paramsrouting.service';
 import { Ng2SearchPipeModule } from 'ng2-search-filter';
-
-
-
-
 import { DashboardComponent } from './admin/dashboard/dashboard.component';
 import { SidebarComponent } from './admin/sidebar/sidebar.component';
 import { MainSectionComponent } from './admin/main-section/main-section.component';
-
-import { HttpClientModule } from '@angular/common/http';
 import { UsersComponent } from './admin/users/users.component';
 import { ProductsComponent } from './admin/products/products.component';
 import { AddComponent } from './admin/add/add.component';
+import { MainHomeComponent } from './home/main-home/main-home.component';
+import { NavbarComponent } from './home/navbar/navbar.component';
+import { FooterComponent } from './home/footer/footer.component';
+import { CarouselComponent } from './home/carousel/carousel.component';
 
 @NgModule({
   declarations: [
@@ -30,14 +30,20 @@ import { AddComponent } from './admin/add/add.component';
     MainSectionComponent,
     UsersComponent,
     ProductsComponent,
-    AddComponent
+    AddComponent,
+    MainHomeComponent,
+    NavbarComponent,
+    FooterComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    NgbModule,
     FormsModule,
     HttpClientModule,
     Ng2SearchPipeModule,
+
   ],
   providers: [ParamsroutingService],
   bootstrap: [AppComponent]
