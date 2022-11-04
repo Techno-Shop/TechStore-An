d15a05d35839f723f1f68181327bda1aea7f6018
khalilbenali1 <khalil.ben.ali16121@gmail.com>
2022-11-04 15:12:24 +0100

    admin interface created,gets users and product data

--- TechStore-An/src/app/app.module.ts
+++ TechStore-An/src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
+import { SidebarComponent } from './admin/sidebar/sidebar.component';
+import { MainSectionComponent } from './admin/main-section/main-section.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import { UsersComponent } from './admin/users/users.component';
+import { ProductsComponent } from './admin/products/products.component';
+import { AddComponent } from './admin/add/add.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    SidebarComponent,
+    MainSectionComponent,
+    UsersComponent,
+    ProductsComponent,
+    AddComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
